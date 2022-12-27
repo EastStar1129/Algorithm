68b5e67ce3dbb02dca43776c80d259d080d2f852
DongGyuJang <cdgu1129@knou.ac.kr>
2022-12-27 21:14:31 +0900

    build : 27일 알고리즘 커밋

--- src/eaststar1129/programers/lv2/date20221226/행렬의곱셈.java
+++ src/eaststar1129/programers/lv2/date20221226/행렬의곱셈.java
@@ -3,21 +3,16 @@ package eaststar1129.programers.lv2.date20221226;
 public class 행렬의곱셈 {
 
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = new int[arr1.length][arr1[0].length];
+        int[][] answer = new int[arr1.length][arr2[0].length];
 
         for (int i=0;i<arr1.length;++i) {
-            for (int j=0;j<arr1[0].length;++j) {
+            for (int j=0;j<arr2[0].length;++j) {
                 for (int k=0;k<arr1[0].length;++k) {
-                    answer[i][j] += arr1[i][k] * getAnInt(j, arr2[k]);
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
                 }
             }
         }
         return answer;
-    }
-
-    private int getAnInt(int j, int[] arr2) {
-
-        return arr2[j];
     }
 
     public static void main(String[] args) {
