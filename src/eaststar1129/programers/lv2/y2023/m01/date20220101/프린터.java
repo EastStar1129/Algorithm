74a6b799857d45dd6d10784e84f23c35ad0e71af
DongGyuJang <cdgu1129@knou.ac.kr>
2023-01-01 23:46:42 +0900

    feat : 2023년 1월 1일 알고리즘 커밋

---
+++ src/eaststar1129/programers/lv2/y2023/m01/date20220101/프린터.java
@@ -0,0 +1,36 @@
+package eaststar1129.programers.lv2.y2023.m01.date20220101;
+
+import java.util.*;
+
+public class 프린터 {
+
+    public int solution(int[] priorities, int location) {
+        int answer = 1;
+
+        PriorityQueue priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int priority : priorities){
+            priorityQueue.add(priority);
+        }
+
+        while(!priorityQueue.isEmpty()){
+            for(int i=0; i<priorities.length; i++){
+                if(priorities[i] == (int)priorityQueue.peek()) {
+                    if(i == location){
+                        return answer;
+                    }
+                    priorityQueue.poll();
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int arr []= {1, 1, 9, 1, 1, 1};
+        int loc = 0;
+        System.out.println(new 프린터().solution(arr , loc));
+    }
+}
